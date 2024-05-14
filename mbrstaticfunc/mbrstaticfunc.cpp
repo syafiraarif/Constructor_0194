218ddb19972ba2b9d3ba499341c56dbcb82aba43
syairaarif <putrisyafira4444@gmail.com>
2024-05-14 10:51:47 +0700

    mendeklarasikan emthod setID dan printAll

--- mbrstaticfunc/mbrstaticfunc.cpp
+++ mbrstaticfunc/mbrstaticfunc.cpp
@@ -22,6 +22,21 @@ public:
 
 };
 
+
+int Mahasiswa::nim = 0;
+
+void Mahasiswa::setID()
+{
+	id = ++nim;
+}
+
+void Mahasiswa::printAll()
+{
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
 int main()
 {
    
