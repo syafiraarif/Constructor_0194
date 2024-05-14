9786ea741049362d1bea4ffe25f0153805b2b7cb
syairaarif <putrisyafira4444@gmail.com>
2024-05-14 10:20:08 +0700

    mendeklarasikan method setID dan method printAll

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -17,6 +17,20 @@ class Mahasiswa
     }
 };
 
+int Mahasiswa::nim = 194;
+
+void Mahasiswa::setID()
+{
+    id = ++nim;
+}
+
+void Mahasiswa::printAll()
+{
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
 int main()
 {
     
