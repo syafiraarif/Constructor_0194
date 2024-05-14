4c68d9277e170872960f817a53b0a99b2551e7b2
syairaarif <putrisyafira4444@gmail.com>
2024-05-14 11:19:31 +0700

    mendeklarasikan constractor diluar class dan didalam class

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -9,13 +9,40 @@ private:
 	int nim;
 	string nama;
 public:
-	Mahasiswa();
-	Mahasiswa(int);
+	Mahasiswa() 
+	{
+		nim = 0;
+		nama = " ";
+	}
+
+	Mahasiswa(int iNim)
+	{
+		nim = iNim;
+	}
+
 	Mahasiswa(string);
 	Mahasiswa(int iNim, string iNama);
 	void cetak();
 };
 
+
+Mahasiswa::Mahasiswa(string Nama)
+{
+	nim = 0;
+	nama = " ";
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama)
+{
+	nim = iNim;
+	nama = iNama;
+}
+
+void Mahasiswa::cetak()
+{
+	cout << endl << "Nim = " << nim << endl;
+	cout << "Nama = " << nama << endl;
+}
 
 int main()
 {
