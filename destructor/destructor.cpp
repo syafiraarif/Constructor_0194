51b119ee0739a9707f2feb95a0533dfc411d1b5a
syairaarif <putrisyafira4444@gmail.com>
2024-05-14 13:09:48 +0700

    membuat class angka

---
+++ destructor/destructor.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+using namespace std;
+
+class angka
+{
+private:
+    int* arr;
+    int panjang;
+public:
+    angka(int);             //constarctor
+    ~angka();               //destructor
+    void cetakData();
+    void isiData();
+};
+
+int main()
+{
+    
+}
+
