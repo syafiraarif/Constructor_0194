c81d555bc12960c551249f52cfa037213a2a72d7
syairaarif <putrisyafira4444@gmail.com>
2024-05-14 11:06:16 +0700

    membuat class mahasiswa

---
+++ konstruktor/konstruktor.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+private:
+	int nim;
+	string nama;
+public:
+	Mahasiswa();
+	Mahasiswa(int);
+	Mahasiswa(string);
+	Mahasiswa(int iNim, string iNama);
+	void cetak();
+};
+
+
+int main()
+{
+
+}
+
